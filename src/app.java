f3d7c2c0758948fcf10b4c2c0c80c060f67b2a4a
manjitkaur1 <kaurmanjit615@gmail.com>
2015-03-09 15:33:11 +1000

    initial commit

---
+++ src/app.java
@@ -0,0 +1,15 @@
+
+public class app {
+
+    public static void  main(String args[]){
+
+        String text="this is a simple test";
+        for(int i=0;i<text.length();++i){
+            System.out.println((text.charAt(i)));
+        }
+        System.out.println();
+    }
+
+}
+
+
